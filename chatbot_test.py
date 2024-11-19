2484264ffdd0367a8c4e814cf0c1821cad0dca52
jayiuk <jayiuk987@gmail.com>
2024-11-19 15:35:38 +0900

    chatbot demo complete

--- chatbot_test.py
+++ chatbot_test.py
@@ -4,12 +4,12 @@ from Retriever import retriever
 import VectorStore
 
 
-model_id = "allganize/Llama-3-Alpha-Ko-8B-Evo"
-llm_model = AutoModelForCausalLM.from_pretrained(model_id)
-llm_tokenizer = AutoTokenizer.from_pretrained(model_id)
+model_id = "Bllossom/llama-3.2-Korean-Bllossom-3B"
+llm_model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = './LLM')
+llm_tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir = './TOKENIZER')
 
 
-test_chatbot_chain = chatbot_chain(model = llm_model, tokenizer = llm_tokenizer, user_id = 'abc123')
+test_chatbot_chain = chatbot_chain(model = llm_model, tokenizer = llm_tokenizer)
 rag_chatbot = test_chatbot_chain.get_chain_with_rag(dir_path='./MLOps_chatbot', collection='testdb', k = 3)
 result = rag_chatbot.invoke("마이너스통장이 뭐야?")
 print(result)
