fe50cded1859a72119eb6171ad71517bd4555dcd
jayiuk <jayiuk987@gmail.com>
2024-11-20 17:45:50 +0900

    modifying prompt & token

--- Chatbot.py
+++ Chatbot.py
@@ -8,6 +8,7 @@ from transformers import pipeline
 from langchain_core.runnables import RunnablePassthrough
 from langchain_huggingface.llms import HuggingFacePipeline
 
+system_prompt = "당신은 한국에 사는 외국인 노동자들을 위한 대출 상담 챗봇입니다. question과 맞는 언어로 답해야 합니다. 만약 question이 태국어로 되어있으면 태국어로 답해야 합니다. question의 언어를 모르는 경우 한국어로 답하세요. 모르는 부분은 retriever를 꼭 활용하세요. 토큰을 다 안써도 되니 답변을 짧고 간결하게 생성하세요. 다음의 예시를 참고해 question에 맞는 답변을 생성하세요. 예시 - 질문:강아지가 뭐야? 답변:강아지는 어린 개를 의미합니다."
 
 class chatbot_chain():
     def __init__(self, model, tokenizer, user_id = None, db_connection = None, *args, **kwargs):
@@ -16,13 +17,13 @@ class chatbot_chain():
         #self.user_id = user_id
         self.prompt = ChatPromptTemplate([
 
-                                            ("system", "당신은 한국에 사는 외국인 노동자들을 위한 대출 상담 챗봇입니다. 사용자가 외국어로 물어봤을 때 해당 언어로 대답해야 합니다. 대답은 짧고 간결하게 해야합니다. 주어진 질문에만 답하세요."),
+                                            ("system", system_prompt),
                                             ("human", '{question}'),
                                             ("assistant", "{retriever}")
                                         ])
 
     
-    def get_llmchain(self):
+    def _get_llmchain(self):
         tokenizer = self.tokenizer
         pad_token = tokenizer.convert_tokens_to_ids("<|end_of_text|>")
         eos_token = tokenizer.convert_tokens_to_ids("<eot_id>")
@@ -35,7 +36,7 @@ class chatbot_chain():
     def get_chain_with_rag(self, dir_path, collection, k):
         vec_db = VectorStore.load_vectorstore(dir_path, collection)
         chain_retriever = retriever(vec_db, searched = k).get_retriever()
-        llm_chain = self.get_llmchain()
+        llm_chain = self._get_llmchain()
         rag_context = {"question" : RunnablePassthrough(), "retriever" : chain_retriever}
         rag_chain = rag_context | llm_chain
         return rag_chain
--- chatbot_test.py
+++ chatbot_test.py
@@ -4,7 +4,7 @@ from Retriever import retriever
 import VectorStore
 import time
 
-start_time = time.time()
+
 model_id = "Bllossom/llama-3.2-Korean-Bllossom-3B"
 llm_model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = './LLM')
 llm_tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir = './TOKENIZER')
@@ -12,7 +12,8 @@ llm_tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir = './TOKENIZER
 
 test_chatbot_chain = chatbot_chain(model = llm_model, tokenizer = llm_tokenizer)
 rag_chatbot = test_chatbot_chain.get_chain_with_rag(dir_path='./MLOps_chatbot', collection='testdb', k = 3)
-result = rag_chatbot.invoke("대출금이 연체되면 어떻게 돼?")
+start_time = time.time()
+result = rag_chatbot.invoke("금리인하요구권이 뭐야?")
 end_time = time.time()
 time_range = end_time - start_time
 print(result)
