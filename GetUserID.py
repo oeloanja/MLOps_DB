dcbd5e02fcaba21f767521450001c10c8f38c656
jayiuk <jayiuk987@gmail.com>
2024-11-21 15:37:48 +0900

    getuserid not tested yet

--- GetUserID.py
+++ GetUserID.py
@@ -11,6 +11,11 @@ db = SQLAlchemy(app)
 @app.route('/api/v1/user_service/users/borrow', methods = ['GET'])
 def GetUserIDBorrow():
     user_id = request.args.get('userID')
+
+    #user_token = request.args.get('user_token')
+    # SECRET Key를 이용해 user 정보가 들어있는 json으로 파싱한 후 user 정보 얻기
+    #user_id = user_token["userid"]
+    
     return user_id
 
 @app.route('/api/users/invest', methods = ['GET'])
