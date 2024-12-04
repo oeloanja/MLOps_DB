e1a029adf4163c11c2089a24126aba0fbea4a6f4
jayiuk <jayiuk987@gmail.com>
2024-12-04 13:23:43 +0900

    deleting unnecessary file

--- testapimodel.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String
-from testapidb import base
-
-class borrow_user(base):
-    __tablename__ = 'borrow_user'
-    user_id = Column(String(255), primary_key = True, index = True)
-    username = Column(String(20), nullable = False)
-    email = Column(String(255), nullable = False)
--- testmain.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from testapi import router
-from testapidb import base, engine
-
-base.metadata.create_all(bind = engine)
-app = FastAPI()
-app.include_router(router)
