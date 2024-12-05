c68fe23a419aeac148909fd6efae6b616ee43977
jayiuk <jayiuk987@gmail.com>
2024-12-05 13:25:06 +0900

    docker build

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, APIRouter
-from flask import Flask, request
+from flask import Flask, request, session, g, Blueprint
 import getuserid
 from langchain_openai import ChatOpenAI
 from agent import LoginAgent, NonLoginAgent
 from dotenv import load_dotenv
 import os
+import asyncio
 
 
 app = Flask(__name__)
@@ -21,31 +22,21 @@ llm_model = ChatOpenAI(
 db = "mysql+pymysql://root:1234@localhost:3306/chat_history"
 
 
-
-@app.route('/borrow', methods = ['GET'])
-def get_borrow_id():
-    return getuserid.GetUserIDBorrow()
-
-@app.route('/invest', methods = ['GET'])
-def get_invest_id():
-    return getuserid.GetUserIDInvest()
-
+@app.route('/')
 def get_id():
-    user_id = get_borrow_id()
-    if user_id is None:
-        user_id = get_invest_id()
-        if user_id is None:
-            return None
+    if "userID" in session:
+        user_id = session["userID"]
+    else:
+        user_id = None
     return user_id
 
 user_id = get_id()
-
 
 login = LoginAgent(llm_model, db_path = db, user_id = user_id)
 non = NonLoginAgent(llm_model)
 
 
-@app.route('/chat/login', methods = ['POST'])
+@bp.route('/login', methods = ['POST'])
 def chat():
     question = request.form['input']
     response = login.answer_to_me(question)
