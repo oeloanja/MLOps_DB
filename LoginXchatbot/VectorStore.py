2fc39d5df0f326e17b53f6347f29764887ed9c4d
jayiuk <jayiuk987@gmail.com>
2024-12-09 15:27:39 +0900

    divide docker

---
+++ LoginXchatbot/VectorStore.py
@@ -0,0 +1,34 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+import os
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+api = os.getenv('OPENAPI')
+os.environ['OPENAI_API_KEY'] = api
+embedding_model = OpenAIEmbeddings(model = "text-embedding-3-small")
+
+
+
+def get_vectorstore(docs, collection, dir_path):
+    vec_store = Chroma.from_documents(
+        collection_name = collection,
+        documents = docs,
+        embedding = embedding_model,
+        persist_directory = dir_path
+    )
+    return vec_store
+
+def add_new_doc(new_docs, vec_db):
+    new_vec_db = vec_db.add_documents(new_docs)
+    return new_vec_db
+
+def load_vectorstore(dir_path, collection_name):
+    persist_db = Chroma(
+        persist_directory = dir_path,
+        embedding_function = embedding_model,
+        collection_name = collection_name
+    )
+    return persist_db
--- LoginXchatbot/app2.py
+++ LoginXchatbot/app2.py
@@ -5,7 +5,6 @@ from agent import LoginAgent, NonLoginAgent
 from dotenv import load_dotenv
 import os
 
-
 app = Flask(__name__)
 
 load_dotenv()
@@ -17,27 +16,12 @@ llm_model = ChatOpenAI(
     max_tokens=2048,
     model_name="gpt-4o-mini"
 )
-db = "mysql+pymysql://root:1234@localhost:3306/chat_history"
 
+chatbot = NonLoginAgent(llm_model)
 
-@app.route('/chat', methods = ['POST'])
-def get_id():
-    response = request.form.get('uuid')
-    return response
-    
-
-user_id = get_id()
-
-chatbot = LoginAgent(llm_model, db_path = db, user_id = user_id)
-
-
-@app.route('/chat/login', methods = ['POST'])
-def chat():
+@app.route('/chat/non', methods = ['POST'])
+def chat_non():
     question = request.form['input']
     response = chatbot.answer_to_me(question)
     print(response['output'])
     return response['output']
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port = 8000)
